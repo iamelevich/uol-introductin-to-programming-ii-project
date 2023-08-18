25c765ca33c31f6a2b66a206c34aac082d4bf0a9
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-08-18 02:19:50 +0200

    refactor: add `FilterWithNumberInput` class
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- src/filters/blur.ts
+++ src/filters/blur.ts
@@ -1,38 +1,18 @@
-import { Filter } from './filter';
+import { FilterWithNumberInput } from './filterNumberInput';
 
 import type { Modal } from '../utils/modal';
 import type { PixelHelper } from '../utils/pixelHelper';
 import type P5 from 'p5';
 
-export class BlurFilter extends Filter {
-  private filterInput: P5.Element | undefined;
-
+export class BlurFilter extends FilterWithNumberInput {
   constructor(p: P5, pixelHelper: PixelHelper, modal: Modal) {
     super(p, pixelHelper, modal, {
+      initialValue: 4,
+      min: 0,
+      max: 10,
       name: 'blur-filter',
       text: 'Blur'
     });
-  }
-
-  protected getModalContent(): string | P5.Element {
-    const content = this.p.createDiv();
-
-    const filterValueElement = this.p.createSpan('4');
-    this.p.createP('Blur: ').parent(content).child(filterValueElement);
-
-    this.filterInput = this.p
-      .createInput('4', 'range')
-      .attribute('min', '1')
-      .attribute('max', '10')
-      .addClass('w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-500')
-      .parent(content);
-
-    this.filterInput.elt.addEventListener('input', (e: InputEvent) => {
-      e.preventDefault();
-      filterValueElement.html(this.filterInput?.value().toString());
-    });
-
-    return content;
   }
 
   apply() {
---
+++ src/filters/filterNumberInput.ts
@@ -0,0 +1,53 @@
+import { Filter } from './filter';
+
+import type { FilterConfig } from './filter';
+import type { Modal } from '../utils/modal';
+import type { PixelHelper } from '../utils/pixelHelper';
+import type P5 from 'p5';
+
+export type FilterWithNumberInputConfig = {
+  initialValue: number;
+  min: number;
+  max: number;
+} & FilterConfig;
+
+export abstract class FilterWithNumberInput extends Filter {
+  protected filterInput: P5.Element | undefined;
+
+  protected initialValue: number;
+  protected min: number;
+  protected max: number;
+
+  constructor(
+    p: P5,
+    pixelHelper: PixelHelper,
+    modal: Modal,
+    { initialValue, min, max, ...baseConfig }: FilterWithNumberInputConfig
+  ) {
+    super(p, pixelHelper, modal, baseConfig);
+    this.initialValue = initialValue;
+    this.min = min;
+    this.max = max;
+  }
+
+  protected getModalContent(): string | P5.Element {
+    const content = this.p.createDiv();
+
+    const filterValueElement = this.p.createSpan('4');
+    this.p.createP(`${this.config.text}: `).parent(content).child(filterValueElement);
+
+    this.filterInput = this.p
+      .createInput(this.initialValue.toString(), 'range')
+      .attribute('min', this.min.toString())
+      .attribute('max', this.max.toString())
+      .addClass('w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-500')
+      .parent(content);
+
+    this.filterInput.elt.addEventListener('input', (e: InputEvent) => {
+      e.preventDefault();
+      filterValueElement.html(this.filterInput?.value().toString());
+    });
+
+    return content;
+  }
+}
