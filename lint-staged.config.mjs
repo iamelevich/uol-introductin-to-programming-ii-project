e34257d2c6a590d746bb008542d1ced07feff19a
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-08-25 19:09:37 +0200

    build: add `lint-staged` and config for it

---
+++ lint-staged.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  // this will check Typescript files
+  '**/*.(ts|tsx)': () => 'tsc --noEmit',
+
+  // This will lint and format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `eslint --fix "${filenames.join('" "')}"`,
+    `prettier --write "${filenames.join('" "')}"`
+  ],
+
+  // this will Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) => `prettier --write "${filenames.join('" "')}"`,
+
+  'package.json': ['prettier-package-json --write', 'git add']
+};
